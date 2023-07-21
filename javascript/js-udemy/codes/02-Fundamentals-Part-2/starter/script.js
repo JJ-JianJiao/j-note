2e76f6013df806132b3b59f9dd24e6d4003756bb
Jiao Jian <jj.jianjiao@gmail.com>
2023-07-21 11:06:34 -0600

    function 07-21

---
+++ javascript/js-udemy/codes/02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,32 @@
+'use strict';
+
+// let hasDriversLicense = false;
+// const passTest = true;
+
+// if (passTest) {
+//     hasDriversLicense = true;
+// }
+// if(hasDriversLicense) {
+//      console.log('I can drive :)');
+// }
+
+// const interface = 'Audio';
+// const private = 331;
+
+
+// functions
+// function logger(){
+//     console.log('my name is jj');
+// }
+
+//calling / running / invoking
+// logger();
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} orranges`;
+    return juice;
+}
+
+const applceJuice = fruitProcessor(5, 0);
+console.log(applceJuice);
