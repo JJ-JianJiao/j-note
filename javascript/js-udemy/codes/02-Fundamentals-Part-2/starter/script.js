11e265e1e8b9fe1a25a0bb9d327f5e567908cd84
Jiao,Jian <jjiao@ab.bluecross.ca>
2023-07-25 16:53:33 -0600

    done function, array, obj review

--- javascript/js-udemy/codes/02-Fundamentals-Part-2/starter/script.js
+++ javascript/js-udemy/codes/02-Fundamentals-Part-2/starter/script.js
@@ -115,3 +115,154 @@
 //const scoreKoalas = calcAverage(23, 34, 27);
 
 // checkWinner(scoreDolphins, scoreKoalas);
+
+
+//Array
+// const friends = ['F1', 'F2', 'F3'];
+
+// console.log(friends);
+
+// const years = new Array(1991, 1984, 2008, 2020);
+// console.log(years);
+
+// console.log(friends[0]);
+// console.log(friends.length);
+// console.log(friends[friends.length - 1]);
+// friends[2] = "new F2";
+// console.log(friends);
+
+// const jj = ['jian', 'jiao', 2037 - 1989, 'programmer', friends];
+// console.log(jj);
+
+//Exercise
+// const calcAge = function (birthYear) {
+//     return 2037 - birthYear;
+// }
+// const years = [1989, 1967, 2022, 2021, 2018];
+
+// const friends = ['F1', 'F2', 'F3'];
+// friends.push('Jay');
+// console.log(friends);
+// friends.pop();
+// console.log(friends);
+// friends.unshift('F0');
+// console.log(friends);
+// friends.shift();
+// console.log(friends);
+
+// const years = [1989, 1967, 2022, 2021, 2018];
+// console.log(years.pop());
+
+// console.log(years.indexOf(2021));
+
+// console.log(years.includes(1694));
+// console.log(years.includes(1967));
+
+/* Write your code below. Good luck! 🙂 */
+
+// const calcTip = function (bill) {
+//     if (bill >= 50 && bill <= 300)
+//         return bill * 0.15;
+//     else
+//         return bill * 0.2;
+// }
+
+// const bills = [125, 555, 44];
+
+// const tips = [];
+// const totals = [];
+
+// for (let i = 0; i < bills.length; i++) {
+//     tips.push(calcTip(bills[i]));
+//     totals.push(tips[i] + bills[i]);
+// }
+
+// console.log(tips);
+// console.log(totals);
+
+//Objects
+// const JJArray = [
+//     'Jian',
+//     'Jiao',
+//     2037 - 1989,
+//     'developer',
+//     ['F1', 'F2', 'F3']
+// ]
+
+// const JJObj = {
+//     firstName: 'Jian',
+//     lastName: 'Jiao',
+//     birthYear: 1989,
+//     job: 'developer',
+//     friends: ['F1', 'F2', 'F3'],
+//     hasDriverLicense: true,
+
+//     //function
+//     // calAge: function (birthYear = this.age) {
+//     //     return 2037 - birthYear;
+//     // }
+//     calAge: function () {
+//         this.age = 2037 - this.birthYear;
+//         return this.age;
+//     },
+//     summaryInfo: function () {
+//         return `${this.firstName} has ${this.friends.length} friends, and his best firend is called ${this.friends[this.friends.length - 1]}`;
+//     }
+// };
+
+// JJObj.Baby = 'Seb';
+// JJObj['wife'] = "Shirley";
+
+// console.log(JJObj.calAge());
+// console.log(JJObj.summaryInfo());
+
+// console.log(JJObj);
+// console.log(JJObj.firstName);
+// console.log(JJObj['firstName']);
+// console.log(JJObj.Baby);
+// console.log(JJObj.wife);
+
+// const interactWithUser = prompt('What do you want to know about JJ? Choose between firstName, lastName, age, job, and firends');
+
+// if (JJObj[interactWithUser]) {
+//     console.log(JJObj[interactWithUser]);
+// }
+
+// console.log(`${JJObj.firstName} has ${JJObj.friends.length} friends, and his best firend is called ${JJObj.friends[JJObj.friends.length - 1]}`);
+// const mark = {
+//     fullName: 'Mark',
+//     mass: 78,
+//     height: 1.69,
+
+//     //Function
+//     calcBMI: function () {
+//         this.bmi = this.mass / (this.height * this.height);
+//         return this.bmi;
+//     },
+//     summary: function () {
+//         return `${this.fullName}'s BMI (${this.bmi})`;
+//     }
+// }
+
+// const john = {
+//     fullName: 'John',
+//     mass: 92,
+//     height: 1.95,
+
+//     //Function
+//     calcBMI: function () {
+//         this.bmi = this.mass / (this.height * this.height);
+//         return this.bmi;
+//     },
+//     summary: function () {
+//         return `${this.fullName}'s BMI (${this.bmi})`;
+//     }
+// }
+
+// mark.calcBMI();
+// john.calcBMI();
+// if (mark.bmi > john.bmi) {
+//     console.log(mark.summary() + " is higher than " + john.summary());
+// } else {
+//     console.log(john.summary() + " is higher than " + mark.summary());
+// }
