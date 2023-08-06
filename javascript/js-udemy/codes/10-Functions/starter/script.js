639ed284f810c2f2ab0a8e5078428be3ff9d4076
Jiao Jian <jj.jianjiao@gmail.com>
2023-08-06 17:53:19 -0600

    bind method: partial applicaiton, with event listensers and basic bind

--- javascript/js-udemy/codes/10-Functions/starter/script.js
+++ javascript/js-udemy/codes/10-Functions/starter/script.js
@@ -107,46 +107,92 @@
 /* The call and apply methods
  **************
  */
-const book = function (flightNum, name) {
-  console.log(
-    `${name} booked a seat on ${this.airline} flight ${this.iataCode} ${flightNum}`
-  );
-  this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
-};
-const lufthansa = {
-  airline: 'Lufthansa',
-  iataCode: 'LH',
-  bookings: [],
-  book,
-};
+// const book = function (flightNum, name) {
+//   console.log(
+//     `${name} booked a seat on ${this.airline} flight ${this.iataCode} ${flightNum}`
+//   );
+//   this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+// };
+// const lufthansa = {
+//   airline: 'Lufthansa',
+//   iataCode: 'LH',
+//   bookings: [],
+//   book,
+// };
 
-lufthansa.book(999, 'Jian');
-lufthansa.book(345, 'John');
-console.log(lufthansa);
+// lufthansa.book(999, 'Jian');
+// lufthansa.book(345, 'John');
+// console.log(lufthansa);
 
-const eurowings = {
-  airline: 'Eurrowngs',
-  iataCode: 'EW',
-  bookings: [],
-};
+// const eurowings = {
+//   airline: 'Eurrowngs',
+//   iataCode: 'EW',
+//   bookings: [],
+// };
 
 // book(23, 'Seb'); //Uncaught TypeError: Cannot read properties of undefined (reading 'airline')
 
 //Call method
-book.call(eurowings, 23, 'Seb');
-book.call(lufthansa, 59, 'Qin');
+// book.call(eurowings, 23, 'Seb');
+// book.call(lufthansa, 59, 'Qin');
 
-const swiss = {
-  airline: 'Swiss Air line',
-  iataCode: 'LX',
-  bookings: [],
-};
+// const swiss = {
+//   airline: 'Swiss Air line',
+//   iataCode: 'LX',
+//   bookings: [],
+// };
 
-book.call(swiss, 584, 'marry');
-console.log(swiss);
+// book.call(swiss, 584, 'marry');
+// console.log(swiss);
 
 //Apply method
-const flightData = [584, 'Xianfa'];
-book.apply(swiss, flightData);
-console.log(swiss);
-book.call(swiss, ...flightData);
+// const flightData = [584, 'Xianfa'];
+// book.apply(swiss, flightData);
+// console.log(swiss);
+// book.call(swiss, ...flightData);
+
+/* bind
+ ***********
+ */
+// const bookEW = book.bind(eurowings);
+// const bookLU = book.bind(lufthansa);
+// const bookSW = book.bind(swiss);
+// bookEW(23, 'JianJiao');
+// bookLU(23, 'JianJiao');
+// bookSW(23, 'JianJiao');
+// console.log(eurowings);
+// console.log(lufthansa);
+// console.log(swiss);
+
+// const bookEW23 = book.bind(eurowings, 23);
+// const bookEW46 = book.bind(eurowings, 46);
+// bookEW23('Xueyan');
+// bookEW23('Lianye');
+// bookEW23('Xianfa');
+// bookEW46('Xiaoliu');
+// bookEW46('Wangyu');
+// console.log(eurowings);
+
+//with event listeners
+// lufthansa.planes = 300;
+// lufthansa.buyPlance = function () {
+//   console.log(this);
+//   this.planes++;
+//   console.log(this.planes);
+// };
+// document
+//   .querySelector('.buy')
+//   .addEventListener('click', lufthansa.buyPlance.bind(lufthansa));
+
+//partial application
+// const addTax = (rate, value) => value + (value * rate) / 100;
+// console.log(addTax(10, 200));
+
+// const addVAT = addTax.bind(null, 15);
+// console.log(addVAT(200));
+
+//challenge
+// const addVTaxRate = rate => value => value + (value * rate) / 100;
+// console.log(addVAT2(200)(15));
+// const addVAT2 = addVTaxRate(15);
+// console.log(addVAT2(200));
