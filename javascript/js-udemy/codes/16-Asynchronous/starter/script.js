2dbdcaf86496b7617c7332802cb3b4dcbcb2fd87
Jiao Jian <jj.jianjiao@gmail.com>
2023-08-24 02:25:05 -0600

    XMLHttpRequest to get api data

--- javascript/js-udemy/codes/16-Asynchronous/starter/script.js
+++ javascript/js-udemy/codes/16-Asynchronous/starter/script.js
@@ -5,12 +5,39 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
-const request = new XMLHttpRequest();
-// request.open('GET','https://restcountries.com/v3.1/all');
-request.open('GET','https://restcountries.com/v3.1/name/china');
-// request.open('GET','https://restcountries.com/v3.1/name/canada');
-request.send();
-request.addEventListener('load',function (e) {  
-    const data = JSON.parse(this.responseText);
-    console.log(data);
-})
+
+const getCountryData = function(country){
+    const request = new XMLHttpRequest();
+    // request.open('GET','https://restcountries.com/v3.1/all');
+    request.open('GET',`https://restcountries.com/v3.1/name/${country}`);
+    // request.open('GET','https://restcountries.com/v3.1/name/canada');
+    request.send();
+    request.addEventListener('load',function (e) {  
+        const countryData = JSON.parse(this.responseText);
+        console.log(countryData);
+        countryData.forEach(country => {
+            const languages = Object.entries(country.languages);
+            const languagesStr = languages.reduce((acc,curr)=> acc + curr[1] + " ",'');
+
+            const currencies = Object.entries(country.currencies);
+            const html = `
+                <article class="country">
+                <img class="country__img" src="${country.flags.svg}" />
+                <div class="country__data">
+                <h3 class="country__name">${country.name.official}</h3>
+                <h4 class="country__region">${country.region}</h4>
+                <p class="country__row"><span>👫</span>${(+country.population / 1000000).toFixed(1)}m people</p>
+                <p class="country__row"><span>🗣️</span>${languagesStr}</p>
+                <p class="country__row"><span>💰</span>${currencies[0][1].name}</p>
+                </div>
+            `;
+            countriesContainer.insertAdjacentHTML('beforeend',html);
+        });
+        countriesContainer.style.opacity = 1;
+    })
+}
+
+// getCountryData('china');
+getCountryData('usa');
+getCountryData('canada');
+getCountryData('portugal');
