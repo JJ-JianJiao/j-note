24bc0ca8d4a66117cd5cac853b4423fc4a06725a
Jiao Jian <jj.jianjiao@gmail.com>
2023-08-30 12:23:15 -0600

    to-level await (ES2022) using await in module without async key words. Module pattern (keep the variable private and using closure), CommonJS Module in Node.js

--- javascript/js-udemy/codes/17-Modern-JS-Modules-Tooling/starter/script.js
+++ javascript/js-udemy/codes/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -2,7 +2,7 @@
 //  import {addToCart, totalPrice as price, tq} from './shoppingCart.js';
 // addToCart('milk',5);
 // console.log(price,tq);
-console.log(`Importing module`);
+// console.log(`Importing module`);
 
 
 //  import * as ShoppingCart from './shoppingCart.js';
@@ -14,11 +14,68 @@ console.log(`Importing module`);
 // add('steak',6);
 // console.log(price,tq);
 
-import add, {cart} from './shoppingCart.js';
- add('steak',6);
- add('milk',2);
- add('apple',4);
- add('banana',10);
+// import add, {cart} from './shoppingCart.js';
+//  add('steak',6);
+//  add('milk',2);
+//  add('apple',4);
+//  add('banana',10);
 
-console.log(cart);
+// console.log(cart);
 
+// console.log('start fetching');
+// const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+// const data = await res.json();
+// console.log(data);
+// console.log('Something');
+
+// const getLastPost = async function () {  
+//     const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+//     const data = await res.json();
+//     return {title: data.at(-1).title, text:data.at(-1).body};
+// }
+
+// const lastPost = await getLastPost();
+// console.log(lastPost);
+
+
+
+//modules pattern
+
+// const ShoppingCartIIFE =  (function () {  
+//     const cart = [];
+//     const shippingCost = 10;
+//     const taotalPrice = 237;
+//     const totalQuantity =23;
+//     const addToCart = function (product, quantity) {  
+//         cart.push({product,quantity});
+//         console.log(`${quantity} ${product} added to cart`);
+//     };
+//     const orderStock = function (product, quantity) {  
+//         cart.push({product,quantity});
+//         console.log(`${quantity} ${product} ordered from supplier`);
+//     };
+
+//     return {
+//         addToCart,
+//         cart,
+//         taotalPrice,
+//         totalQuantity,
+//     }
+// })();
+
+
+// ShoppingCartIIFE.addToCart('apple',4);
+// ShoppingCartIIFE.addToCart('pizza',2);
+// console.log(ShoppingCartIIFE);
+
+
+
+//CommonJS modules
+//Export
+// export.addToCart =  function (product, quantity) {  
+//     cart.push({product,quantity});
+//     console.log(`${quantity} ${product} added to cart`);
+// };
+
+// //Inport
+// const {addToCart} = require('./shoppingCart.js');
