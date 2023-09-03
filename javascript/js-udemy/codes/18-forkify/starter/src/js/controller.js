5147c27a6c3d4a1cc31496ba42ce3c14f0e2716e
caixueyan <caixueyan@caixueyan-MacOS.lan>
2023-09-02 19:52:51 -0600

    add search, show results in search bar and display next and prev buttons, TODO: next and prev btns feature

--- javascript/js-udemy/codes/18-forkify/starter/src/js/controller.js
+++ javascript/js-udemy/codes/18-forkify/starter/src/js/controller.js
@@ -1,4 +1,9 @@
 const recipeContainer = document.querySelector('.recipe');
+const searchBtn = document.querySelector('.search__btn');
+const searchResultBar = document.querySelector('.search-results .results');
+const prevBtn = document.querySelector('.pagination__btn--prev');
+const nextBtn = document.querySelector('.pagination__btn--next');
+console.log(searchResultBar,prevBtn,nextBtn);
 
 const timeout = function (s) {
   return new Promise(function (_, reject) {
@@ -10,23 +15,87 @@ const timeout = function (s) {
 
 // https://forkify-api.herokuapp.com/v2
 // API key: 90dc691a-882e-4620-bdc8-edd5ed5dc4f0
-
+//https://forkify-api.herokuapp.com/api/v2/recipes?search=pizza&key=<insert your key>
 
 ///////////////////////////////////////
 
-console.log("Test");
+// console.log("Test");
+// const searchRecipe = 'pizza';
+const APIKey = '90dc691a-882e-4620-bdc8-edd5ed5dc4f0';
 
-const urlExample = `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`
+const hideSearchResultsBarBtns = function () {  
+  prevBtn.style.display="none";
+  nextBtn.style.display="none";
+};
+
+const showSearchResultsBarBtns = function () {  
+  prevBtn.style.display="block";
+  nextBtn.style.display="block";
+};
+
+const setSearchResultsBarBtns = function (pre, next = pre + 2) {  
+  prevBtn.querySelector('span').textContent = `Page ${pre}`;
+  nextBtn.querySelector('span').textContent = `Page ${next}`;
+};
+
+const inital = function(){
+  //hide searchResultBarBtns
+  hideSearchResultsBarBtns();
+};
+
+searchBtn.addEventListener('click',function (e) {  
+  e.preventDefault();
+  searchResultBar.innerHTML = '';
+  hideSearchResultsBarBtns();
+  const searchContent = document.querySelector('.search__field').value;
+  // console.log(searchContent);
+  const url = `https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchContent}&key=${APIKey}`;
+  getRecipe(url);
+});
+
+inital();
+
+// const urlExample = `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`
+// const urlExample = `https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchRecipe}&key=${APIKey}`;
 const getRecipe = async function(url){
   try{
     const recipePromise = await fetch(url);
     const recipe = await recipePromise.json();
     // console.log(recipe);
+    if(recipe.status !== 'success')
+      throw new Error('Get recipe results fail ❤️‍🩹❤️‍🩹❤️‍🩹❤️‍🩹');
+    
+    const recipeData = recipe.data.recipes;
+    const svgUser = `./src/img/icons.svg#icon-user`;
+    for(let i = 0; i <=9; ++i){
+      // console.log(recipeData[i]);
+      const searchBarItemHtml = `
+        <li class="preview">
+          <a class="preview__link preview__link--active" href="#23456">
+            <figure re class="preview__fig">
+              <img src="${recipeData[i].image_url}" alt="Test" />
+            </figure>
+            <div class="preview__data">
+              <h4 class="preview__title">${recipeData[i].title}</h4>
+              <p class="preview__publisher">${recipeData[i].publisher}</p>
+              <div class="preview__user-generated">
+                <svg>
+                  <use href="${svgUser}"></use>
+                </svg>
+              </div>
+            </div>
+          </a>
+        </li>
+      `;
+      searchResultBar.insertAdjacentHTML('beforeend',searchBarItemHtml);
+    };
+    setSearchResultsBarBtns(1);
+    showSearchResultsBarBtns();
     return recipe;
   }catch(err){
     console.log(err);
   }
 };
 
-const recipe = getRecipe(urlExample);
+// const recipe = getRecipe(urlExample);
 // console.log(recipe.then());
