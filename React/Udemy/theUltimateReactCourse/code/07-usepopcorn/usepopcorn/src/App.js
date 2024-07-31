a01177a59a5895e3ac861b432138618375d2ba7f
Jiao Jian <jj.jianjiao@gmail.com>
2024-07-30 21:58:13 -0600

    done the UI part

--- React/Udemy/theUltimateReactCourse/code/07-usepopcorn/usepopcorn/src/App.js
+++ React/Udemy/theUltimateReactCourse/code/07-usepopcorn/usepopcorn/src/App.js
@@ -252,9 +252,51 @@ function Button({ isOpen, setIsOpen }) {
 }
 
 function MovieDetails({ selectedID, onCloseMovie }) {
+  const [movie, setMovie] = useState({});
+
+  const { Title: title, Year: year, Poster: poster, Runtime: runtime, imdbRating, Plot: plot, Released: released, Actors: actors, Director: director, genre } = movie;
+
+
+  console.log(title, year);
+
+  useEffect(function () {
+    async function getMovieDetails() {
+      const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&i=${selectedID}`);
+      const data = await res.json();
+      console.log(data);
+      setMovie(data);
+    }
+    getMovieDetails();
+  }, [])
+
   return <div className="details">
-    <button className="btn-back" onClick={onCloseMovie}>&larr;</button>
-    {selectedID}
+    <header>
+
+      <button className="btn-back" onClick={onCloseMovie}>&larr;</button>
+      <img src={poster} alt={`Poster of ${movie} movie`} />
+      <div className="details-overview">
+        <h2>{title}</h2>
+        <p>
+          {released} &bull; {runtime}
+        </p>
+        <p>{genre}</p>
+        <p>
+          <span>⭐</span>
+          {imdbRating} IMDB rating
+        </p>
+      </div>
+    </header>
+
+    <section>
+      <div className="rating">
+      <StartRating maxRating={10} size={24}/>
+      </div>
+      <p>
+        <em>{plot}</em>
+      </p>
+      <p>Starring {actors}</p>
+      <p>Directed by {director}</p>
+    </section>
   </div>
 }
 
